1d80866303b277a846152af6fe4b654ea4f42fbb
Kevin Gao <gaojunxuanbox@live.com>
2018-09-07 20:24:13 -0500

    edit deploy script

--- deploy.cmd
+++ deploy.cmd
@@ -120,7 +120,7 @@ IF NOT EXIST "%DEPLOYMENT_TARGET%\env\azure.env.%PYTHON_RUNTIME%.txt" (
 
 :: 4. Install packages
 echo Pip install requirements.
-env\scripts\pip install -r requirements.txt
+D:\home\python364x64\python.exe -m pip install -r requirements.txt
 IF !ERRORLEVEL! NEQ 0 goto error
 
 REM Add additional package installation here
